79c850e6c0abe3240b484c93805b2f24ca5b088d
Arielton97 <arieltonribeiro97@gmail.com>
2024-12-16 20:59:42 -0300

    alteração: adicionando o método construtor na classe Account

--- src/account/AccountTest.java
+++ src/account/AccountTest.java
@@ -1,30 +1,17 @@
 package account;// account.AccountTest.java
-// Cria e manipula um objeto account.Account
+// Usando o construtor de Account para inicializar a instância name
+// variável no momento em que cada objeto Account é criado.
 
 public class AccountTest
 {
     public static void main(String[] args)
     {
         // cria um objeto Scanner para obter entrada a partir da janela de comando
-        java.util.Scanner input = new java.util.Scanner(System.in);
+        Account account1 = new Account("Jane Gren");
+        Account account2 = new Account("John Blue");
 
-        // cria um objeto account.Account e o atribui a myAccount
-        //Account myAccount = new Account();
-        Account account1 = new Account("Jane Green");
-
-        // exibe o valor inicial do nome (null)
-        System.out.printf("Initial name is: %s%n%n", account1.getName());
-
-        // solicita e lê o nome
-        System.out.println("Please enter the name: ");
-        String theName = input.nextLine(); // lê uma linha de texto
-        account1.setName(theName); // insere theName em myAccount
-        System.out.println(); // gera a saída de uma linha em branco
-
-        // exibe o nome armazenado no objeto myAccount
-        System.out.printf("Name in object myAccount is:%n%s%n",
-                account1.getName());
-
-        input.close();
+        // exibe o valor inicial de nome para cada Account
+        System.out.printf("account1 name is: %s%n", account1.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
     }
-} // fim da classe account.AccountTest
+} // fim da classe AccountTest
